2a72c15cd79c46944470699402b0218b7dd68f05
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-04-03 01:20:02 +0530

    Todo App

--- todo-list-app/src/TodoList.jsx
+++ todo-list-app/src/TodoList.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
-  let [todos, setTodos] = useState(["Sample Task"]);
-  let [newTodo, setNewTodo] = useState([""]);
+  // let [todos, setTodos] = useState(["Sample Task"]);
+  let [todos, setTodos] = useState([{ task: "sample-task", id: uuidv4(), isDone: false }]);
+  let [newTodo, setNewTodo] = useState("");
 
   let addTask = () => {
     // console.log("We have added a task.");
-    setTodos([...todos, newTodo]);
+    // setTodos([...todos, newTodo]);
+    // setTodos([...todos, {task: newTodo, id: uuidv4()}]);
+    setTodos((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4(), isDone: false }];
+    });
     setNewTodo("");
     // console.log(todos);
   };
@@ -16,9 +22,79 @@ export default function TodoList() {
     setNewTodo(event.target.value);
   };
 
+
+  let deleteTodo = (id) => {
+    // console.log("Delete Todo");
+    // console.log(id);
+
+    //  let copy = todos.filter((todo) => todo.id != id);
+    //  console.log(copy);
+
+    // setTodos(todos.filter((todo) => todo.id != id));
+
+    setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+  }
+
+
+  // let upperCaseAll = () => {
+  //   setTodos((prevTodos) => (
+  //     prevTodos.map((todo) => {
+  //       return {
+  //         ...todo,
+  //         task: todo.task.toUpperCase(),
+  //       };
+  //     })
+  //   ));
+  // };
+
+
+  // let upperCaseOne = (id) => {
+  //   setTodos((prevTodos) => (
+  //     prevTodos.map((todo) => {
+  //       if (todo.id === id) {
+  //         return {
+  //           ...todo,
+  //           task: todo.task.toUpperCase(),
+  //         };
+  //       } else {
+  //         return todo;
+  //       }
+  //     })
+  //   ));
+  // };
+
+
+  let markAsDone = (id) => {
+    setTodos((prevTodos) => (
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    ));
+  };
+
+
+  let markAllDone = () => {
+    setTodos((prevTodos) => (
+      prevTodos.map((todo) => {
+        return {
+          ...todo,
+          isDone: true,
+        };
+      })
+    ));
+  };
+
+
   return (
     <div>
-        <h1>Todo List App</h1>
+      <h1>Todo List App</h1>
       <input placeholder="Add a task.." value={newTodo} onChange={updateTodo} />
       <br />
       <br />
@@ -30,9 +106,21 @@ export default function TodoList() {
       <h4>Todo Task</h4>
       <ul>
         {todos.map((todo) => (
-          <li>{todo}</li>
+          // <li>{todo}</li>
+          // <li key={todo.id}>{todo.task}</li>
+          <li key={todo.id}>
+            <span style={todo.isDone ? {textDecorationLine: "line-through"} : {}}>
+              {todo.task}
+            </span>
+            &nbsp; &nbsp; &nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            {/* <button onClick={() => upperCaseOne(todo.id)}>UpperCase One</button> */}
+            <button onClick={() => markAsDone(todo.id)}>Mark As Done</button>
+          </li>
         ))}
       </ul>
+      <br />
+      <button onClick={markAllDone}>Mark All As Done</button>
     </div>
   );
 }

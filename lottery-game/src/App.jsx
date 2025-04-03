4219f8b4698fc995781a03490044bca8c9b68536
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-04-04 02:42:57 +0530

    Lottery Game

--- lottery-game/src/App.jsx
+++ lottery-game/src/App.jsx
@@ -1,9 +1,51 @@
+// import './App.css'
+// import Lottery from './Lottery'
+// import Ticket from './Ticket'
+// import TicketNum from './TicketNum'
+
+// function App() {
+
+//   return (
+//     <>
+//       {/* <Lottery /> */}
+//       {/* <TicketNum num={5}/>
+//       <TicketNum num={4}/>
+//       <TicketNum num={2}/> */}
+
+//       {/* <Ticket ticket={[0, 1, 2]}/>
+//       <Ticket ticket={[5, 6, 7]}/> */}
+
+//       <Lottery n={3} winningSum={15}/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
 import './App.css'
 import Lottery from './Lottery'
 import Ticket from './Ticket'
 import TicketNum from './TicketNum'
+import { sum } from "./helper"
 
 function App() {
+
+  // let winCondition = (ticket) => {
+  //   return sum(ticket) === 15;
+  // }
+
+  // let winCondition = (ticket) => {
+  //     return ticket.every((num) => num === ticket[0]);
+  //   }
+
+  let winCondition = (ticket) => {
+    return ticket[0] === 0;
+  }
 
   return (
     <>
@@ -15,7 +57,7 @@ function App() {
       {/* <Ticket ticket={[0, 1, 2]}/>
       <Ticket ticket={[5, 6, 7]}/> */}
 
-      <Lottery n={3} winningSum={15}/>
+      <Lottery n={3} winCondition={winCondition}/>
     </>
   )
 }
--- lottery-game/src/Lottery.jsx
+++ lottery-game/src/Lottery.jsx
@@ -37,16 +37,46 @@
 
 
 
+// import { useState } from "react"
+// import { generateTicket, sum } from "./helper";
+// import Ticket from "./Ticket";
+
+
+// export default function Lottery({n = 3, winningSum = 15}) {
+
+//     let [ticket, setTicket] = useState(generateTicket(3));
+
+//     let isWinning = sum(ticket) === winningSum;
+
+//     const handleBuyNewTicket = () => {
+//         setTicket(generateTicket(n));
+//     }
+
+//     return (
+//         <div>
+//         <h1>Lottery Game!</h1>
+//         <Ticket ticket={ticket}/>
+//         <button onClick={handleBuyNewTicket}>Buy New Ticket</button>
+//         <h3>{isWinning && "Congratulations 🎉, you won!"}</h3>
+//         </div>
+//     )
+// }
+
+
+
+
+
+
 import { useState } from "react"
 import { generateTicket, sum } from "./helper";
 import Ticket from "./Ticket";
 
 
-export default function Lottery({n = 3, winningSum = 15}) {
+export default function Lottery({ n = 3, winCondition }) {
 
-    let [ticket, setTicket] = useState(generateTicket(3));
+    let [ticket, setTicket] = useState(generateTicket(n));
 
-    let isWinning = sum(ticket) === winningSum;
+    let isWinning = winCondition(ticket);
 
     const handleBuyNewTicket = () => {
         setTicket(generateTicket(n));
